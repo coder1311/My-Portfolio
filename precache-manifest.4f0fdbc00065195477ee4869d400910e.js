0313076cc00cd337eeb706f45a1696031beb6771
coder1311 <1311ritu.raj@gmail.com>
2021-06-27 16:49:24 +0530

    Updates

---
+++ precache-manifest.4f0fdbc00065195477ee4869d400910e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0a4e4a09e592a304de7",
+    "url": "/My-Portfolio/static/css/main.dc2233fe.chunk.css"
+  },
+  {
+    "revision": "f0a4e4a09e592a304de7",
+    "url": "/My-Portfolio/static/js/main.f0a4e4a0.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/My-Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "aa9d0237064e9f092376",
+    "url": "/My-Portfolio/static/js/runtime~main.aa9d0237.js"
+  },
+  {
+    "revision": "68ba741cd0fc590f3f98ea66cdcf10ac",
+    "url": "/My-Portfolio/index.html"
+  }
+];

98a4474d3d332700e89d50de9c668214d69d0048
coder1311 <1311ritu.raj@gmail.com>
2021-06-27 15:52:13 +0530

    Initial Commit

---
+++ src/resumeData.js
@@ -0,0 +1,101 @@
+let resumeData = {
+    "imagebaseurl":"my-url",
+    "name": "Ritu Raj",
+    "role": "Competitive Coder | MERN Stack Developer | Problem Solver",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": "I love to explore & learn about new technologies and do problem solving on any coding platform or play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/ritu-raj13/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/coder1311",
+          "className":"fa fa-github"
+        },
+
+      ],
+    "aboutme":"I am currently a pre-final year B.Tech student at Netaji Subhash Engineering College,Kolkata. I am a self taught Problem Solver, Coder and MERN Stack Web Developer, currently diving deeper into BlockChain Technology. I believe that to be successful in life, one needs to be a lifelong learner and obsessive with their dreams & keep working towards them.",
+    "address":"Bihar Sharif, Bihar, India",
+    "website":"my-url",
+    "education":[
+      {
+        "UniversityName":"Netaji Subhash Engineering College, Kolkata",
+        "specialization":"B.Tech - Computer Science & Engineering",
+        "MonthOfPassing":"2018 -",
+        "YearOfPassing":"2022",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Columbia Convent School, Indore",
+        "specialization":"Intermediate",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2017",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Best Enlist",
+        "specialization":"React Web Developer",
+        "MonthOfLeaving":"Nov 2020 -",
+        "YearOfLeaving":"Dec 2020",
+        "Achievements":"Certificate of Excellence & Letter of Recommendation"
+      },
+      {
+        "CompanyName":"Coding Tutor",
+        "specialization":"Coding Instructor",
+        "MonthOfLeaving":"Jan 2020 -",
+        "YearOfLeaving":"Mar 2020",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Problem Solving - DSA"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+
+  export default resumeData
